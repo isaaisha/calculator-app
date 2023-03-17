126b7a97cd07bb7a3bff7515c4545baaa120fdaf
isaaisha <aishaaryani7@gmail.com>
2023-03-17 14:51:50 +0700

    menambahkan file calculator

---
+++ calculator.cs
@@ -0,0 +1,20 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		
+		
+		
+		
+	}
+	ststic int penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	
+	static in pengurangan (int a, int b)
+	}
+		return a-b;
+	}
+}
